fc4fad8419ca99bbe3aa67c24214e2bc6f50d4d9
Ding Ruiqi <416640656@qq.com>
2020-09-13 12:11:34 +0800

    。。

---
+++ yingxue/precache-manifest.7eb77e581cb7ddde62d3a21b96feb0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1312b34f5d5171d1e77875ed1d87b5b4",
+    "url": "/yingxue/index.html"
+  },
+  {
+    "revision": "2ae9fecc82012f9d58b4",
+    "url": "/yingxue/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb84a36b0ffb3898eac9",
+    "url": "/yingxue/static/js/2.b67e83e3.chunk.js"
+  },
+  {
+    "revision": "b12473e1ae5c8a8b7d1bac97380c3f44",
+    "url": "/yingxue/static/js/2.b67e83e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae9fecc82012f9d58b4",
+    "url": "/yingxue/static/js/main.ae1570c9.chunk.js"
+  },
+  {
+    "revision": "b8158f2a36a2566f8612",
+    "url": "/yingxue/static/js/runtime-main.981f4f28.js"
+  }
+]);
