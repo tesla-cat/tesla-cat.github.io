80bd8c0f68fe58f521d5bdb542ac98f2cf36acb3
Rick (Ruiqi) Ding <416640656@qq.com>
2021-04-14 01:12:24 +0800

    。。

--- src/mkdocs/docs/extra.js
+++ src/mkdocs/docs/extra.js
@@ -4,12 +4,13 @@ document.head.appendChild(script)
 
 function loadMD(pdbFile, xtcFile, atomPair){
   var stage = new NGL.Stage("ngl")
+  const root = 0 ? '': '/site'
   window.addEventListener("resize", e=>stage.handleResize(), false)
-  stage.loadFile(pdbFile).then((o)=>{    
+  stage.loadFile(`${root}/files/md/${pdbFile}`).then((o)=>{    
     o.addRepresentation('cartoon', { sele: 'protein' })
     o.addRepresentation('distance', { atomPair })
     o.autoView()
-    NGL.autoLoad(xtcFile).then((frames)=>{
+    NGL.autoLoad(`${root}/files/md/${xtcFile}`).then((frames)=>{
       o.addTrajectory(frames)
       o.trajList[0].trajectory.player.play()
     })
--- src/mkdocs/docs/genetically encoded voltage indicator simulation.md
+++ src/mkdocs/docs/genetically encoded voltage indicator simulation.md
@@ -2,6 +2,6 @@
 <div id="ngl" style="width:100%; height:500px;"></div>
 <script>
 setTimeout(()=>{ 
-  loadMD('/files/md/ASAP3.pdb', '/files/md/ASAP3.xtc', [["15.CA", "365.CA"]])
+  loadMD('ASAP3.pdb', 'ASAP3.xtc', [["15.CA", "365.CA"]])
 }, 500)
 </script>
