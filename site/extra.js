c9075f8530b5ad1b069cbe596533630571422696
Rick (Ruiqi) Ding <416640656@qq.com>
2021-04-14 01:04:57 +0800

    。。

---
+++ site/extra.js
@@ -0,0 +1,17 @@
+var script = document.createElement('script')
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/ngl/2.0.0-dev.38/ngl.js'
+document.head.appendChild(script)
+
+function loadMD(pdbFile, xtcFile, atomPair){
+  var stage = new NGL.Stage("ngl")
+  window.addEventListener("resize", e=>stage.handleResize(), false)
+  stage.loadFile(pdbFile).then((o)=>{    
+    o.addRepresentation('cartoon', { sele: 'protein' })
+    o.addRepresentation('distance', { atomPair })
+    o.autoView()
+    NGL.autoLoad(xtcFile).then((frames)=>{
+      o.addTrajectory(frames)
+      o.trajList[0].trajectory.player.play()
+    })
+  })
+}
