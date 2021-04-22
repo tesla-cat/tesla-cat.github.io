ee99218138459ddf7c1f080e47c5cd87e2df2483
Rick (Ruiqi) Ding <416640656@qq.com>
2021-04-23 01:28:20 +0800

    。。

--- src/doge/Messari/Messari.py
+++ src/doge/Messari/Messari.py
@@ -1,12 +1,15 @@
 
 import json, os, pathlib, time
 import urllib.request
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
-metricsJSON = r'C:\Users\rick\Desktop\doge\Messari\metrics.json'
 folder = pathlib.Path(__file__).parent.absolute()
+metricsJSON = os.path.join(folder, 'metrics.json')
 
 def getData(assetKey: str = 'dogecoin'):
-    resultsFile = os.path.join(folder, f'results-{assetKey}.json')
+    resultsFile = os.path.join(folder, f'{assetKey}.json')
     try:
         with open(resultsFile, 'r') as f:
             results = json.load(f)
@@ -22,7 +25,7 @@ def getData(assetKey: str = 'dogecoin'):
             try:
                 print(metric_id)
                 if metric_id not in results:
-                    url = f'https://data.messari.io/api/v1/assets/{assetKey}/metrics/{metric_id}/time-series'
+                    url = f'https://data.messari.io/api/v1/assets/{assetKey}/metrics/{metric_id}/time-series?interval=1d&start=2020-08-12&end=2021-04-21'
                     results[metric_id] = json.loads( urllib.request.urlopen(url).read().decode("utf-8") )
                     time.sleep(1)
                 break
@@ -32,13 +35,42 @@ def getData(assetKey: str = 'dogecoin'):
                 time.sleep(10)
 
 def fixData(assetKey: str = 'dogecoin'):
-    resultsFile = os.path.join(folder, f'results-{assetKey}.json')
+    resultsFile = os.path.join(folder, f'{assetKey}.json')
     with open(resultsFile, 'r') as f:
         results = json.load(f)
     for metric_id, result in results.items():
         results[metric_id] = json.loads( result )
     with open(resultsFile, 'w+') as f:
         json.dump(results, f)
-            
+
+def cleanData(assetKey: str = 'dogecoin'):
+    resultsFile = os.path.join(folder, f'{assetKey}.json')
+    with open(resultsFile, 'r') as f:
+        results = json.load(f)
+    cleaned = []
+    for metric_id, result in results.items():
+        columns = result['data']['parameters']['columns']
+        description = result['data']['schema']['description']
+        values_schema = result['data']['schema']['values_schema']
+        values = result['data']['values']
+        if values is None:
+            continue
+        assert columns[0] == 'timestamp'
+        array = np.swapaxes(np.array(values), 0, 1)
+        x = (array[0]/1000).astype('int')
+        x = [datetime.utcfromtimestamp(xi).strftime('%Y-%m-%d') for xi in x]
+        print(x[0], x[-1])
+        for i, y in enumerate(array[1:]):
+            try:
+                plt.plot(x, normalize(y), label=f'{columns[1:][i]}')
+            except:
+                pass
+    plt.legend()
+    plt.show()
+
+def normalize(a):
+    return (a - np.min(a)) / np.ptp(a)
+
 #getData()
-fixData()
+cleanData()
+
